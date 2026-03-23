702f946b31f2dffc48e9628304f775266420dd87
KseniiaP84 <oksanaprohura@gmail.com>
2026-03-23 11:45:57 +0200

    Update SpeakHello.js

--- Lab3/SpeakHello.js
+++ Lab3/SpeakHello.js
@@ -1,1 +1,12 @@
+(function (window) {
+  var speakWord = "Hello";
 
+  function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = {
+    speak: speak
+  };
+
+})(window);
